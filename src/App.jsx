73974c262076f6d2df4c097240cf4f685ebfff53
Aeltiel <ingrid.stemer@live.fr>
2023-09-01 12:32:30 +0200

    create app Tv show

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="main_container">
+      <div className="header">
+        <div className="row">
+          <div className="col-4">
+            <div>Logo</div>
+            <div>Subtitle</div>
+          </div>
+          <div className="col-sm-12 col-md-4">
+            <input type="text" />
+          </div>
+        </div>
+      </div>
+      <div className="tvShowSetails">Détails</div>
+      <div className="recommandations">Recommandations</div>
+    </div>
+  );
+}
+
+export default App;
